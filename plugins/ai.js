4f5535c8d7112d9f7fda576b08485b6646cf3439
inrl-md <109638623+inrl-md@users.noreply.github.com>
2024-03-01 06:06:59 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -9,6 +9,18 @@ const {
 
 
 inrl({
+	pattern: 'diffusion ?(.*)',
+	type: "eva",
+	desc: "stable diffusion ai",
+	fromMe: mode
+}, async (message, match) => {
+	match = match || message.reply_message.text;
+	if (!match) return await message.send("*please give me an query!*");
+	const res = await getBuffer(`${config.BASE_URL}api/ai/diffusion?text=${match}&apikey=${config.INRL_KEY}`);
+	return await message.send(res, {},'image');
+});
+
+inrl({
 	pattern: 'gemini ?(.*)',
 	type: "eva",
 	fromMe: mode,
