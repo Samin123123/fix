49a1d7cad0146637a18f37f04a5b2cd6a0edb426
inrl-md <109638623+inrl-md@users.noreply.github.com>
2024-03-11 23:37:45 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -1,26 +1,17 @@
 const {
     inrl,
     mode,
-    sendPhoto,
-    sendVoice,
-    sendGif,
-    sendBassAudio,
-    sendSlowAudio,
-    sendBlownAudio,
-    sendDeepAudio,
-    sendErrapeAudio,
-    sendFastAudio,
-    sendFatAudio,
-    sendNightcoreAudio,
-    sendReverseAudio,
-    sendSquirrelAudio,
     toAudio,
     toPTT,
     toVideo,
     AudioMetaData,
     lang,
-    config
+    config,
+    webp2mp4File
 } = require('../lib');
+
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
 
 inrl({
     pattern: 'photo ?(.*)',
@@ -30,20 +21,16 @@ inrl({
 }, async (message) => {
     if (!message.reply_message.sticker) return  await message.reply(lang.BASE.NEED.format("non animated sticker message"));
     if(message.reply_message.isAnimatedSticker) return  await message.reply(lang.BASE.NEED.format("please reply to a non animated sticker"));
-    return await sendPhoto(message);
-});
-inrl({
-    pattern: 'mp4 ?(.*)',
-    desc: lang.CONVERTER.VIDEO_DESC,
-    type: "converter",
-    fromMe: mode
-}, async (message, match) => {
-    if (!message.reply_message.sticker) return message.reply(lang.BASE.NEED.format("animated sticker message"));
-    if(!message.reply_message.isAnimatedSticker) return  await message.reply(lang.BASE.NEED.format("please reply to an animated sticker"));
-    let media = await toVideo(await message.reply_message.download())
-    return await message.send(media,{
-        mimetype: 'video/mp4',
-    },'video')
+    const media = await message.client.downloadAndSaveMediaMessage(message.reply_message.sticker)
+        await ffmpeg(media)
+            .fromFormat('webp_pipe')
+            .save('output.png')
+            .on('error', function(err) {
+                return await message.send(`*error while converting webp to image*`);
+            })
+            .on('end', async () => {
+                return await message.send(fs.readFileSync('output.png'), {},'image');
+            });
 });
 inrl({
     pattern: 'voice ?(.*)',
@@ -64,9 +51,9 @@ inrl({
     type: "converter",
     fromMe: mode
 }, async (message) => {
-    
     if (!message.reply_message.sticker || message.reply_message.video) return message.reply(lang.BASE.NEED.format("animated sticker/video message"));
-    return await sendGif(message)
+    await webp2mp4File(await message.client.downloadAndSaveMediaMessage(message.reply_message.sticker || message.reply_message.video))
+    return await message.send({ url : webpToMp4.result }, {gifPlayback: true, quoted: message.data }, 'video'); 
 });
 inrl({
     pattern: 'bass ?(.*)',
@@ -75,7 +62,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendBassAudio(message)
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-af equalizer=f=54:width_type=o:width=2:g=20"])
+        .save("./media/bass.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating bass audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/bass.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+        });
 });
 inrl({
     pattern: 'slow ?(.*)',
@@ -84,7 +83,20 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendSlowAudio(message)
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .audioFilter("atempo=0.5")
+        .outputOptions(["-y", "-af", "asetrate=44100*0.9"])
+        .save("./media/slow.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while degreasing audio speed*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/slow.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+      });
 });
 inrl({
     pattern: 'blown ?(.*)',
@@ -93,7 +105,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendBlownAudio(message)
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-af acrusher=.1:1:64:0:log"])
+        .save("./media/blown.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating blown audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/blown.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+        });
 });
 inrl({
     pattern: 'deep ?(.*)',
@@ -102,7 +126,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendDeepAudio(message);
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-af atempo=4/4,asetrate=44500*2/3"])
+        .save("./media/bass.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating bass audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/bass.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+      });      
 });
 inrl({
     pattern: 'earrape ?(.*)',
@@ -111,7 +147,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendErrapeAudio(message)
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-af volume=12"])
+        .save("./media/bass.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating bass audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/bass.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+        });	
 });
 inrl({
     pattern: 'fast ?(.*)',
@@ -120,7 +168,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendFastAudio(message)
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-filter:a atempo=1.63,asetrate=44100"])
+        .save("./media/bhass.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating bass audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/bhass.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+     });
 });
 inrl({
     pattern: 'fat ?(.*)',
@@ -129,7 +189,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendFatAudio(message);
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-filter:a atempo=1.6,asetrate=22100"])
+        .save("./media/bgass.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating bass audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/bgass.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+        });
 });
 inrl({
     pattern: 'nightcore ?(.*)',
@@ -138,7 +210,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendNightcoreAudio(message);
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-filter:a atempo=1.06,asetrate=44100*1.25"])
+        .save("./media/bgass.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating bass audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/bgass.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+        });  
 });
 inrl({
     pattern: 'reverse ?(.*)',
@@ -147,7 +231,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendReverseAudio(message);
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-filter_complex areverse"])
+        .save("./media/bgass.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating bass audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/bgass.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+        });  
 });
 inrl({
     pattern: 'squirrel ?(.*)',
@@ -156,7 +252,19 @@ inrl({
     fromMe: mode
 }, async (message) => {
     if (!message.reply_message.audio) return message.reply(lang.BASE.NEED.format("audio message"));
-    return await sendSquirrelAudio(message);
+    await ffmpeg(await message.client.downloadAndSaveMediaMessage(message.reply_message.audio))
+        .outputOptions(["-filter:a atempo=0.5,asetrate=65100"])
+        .save("./media/beass.mp3")
+        .on('error', function(err) {
+                return await message.send(`*error while generating bass audio*`);
+         })
+        .on('end', async () => {
+            await message.client.sendMessage(message.from, {
+                audio: fs.readFileSync('./media/beass.mp3'),
+                mimetype: 'audio/mp4',
+                ptt: false
+            })
+        });
 });
 
 inrl({
