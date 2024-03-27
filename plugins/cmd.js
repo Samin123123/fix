fd4716df75b9fc202713e8fb0bd2a8e1f6df0eec
inrl-md <109638623+inrl-md@users.noreply.github.com>
2024-03-27 08:25:58 +0530

    Update cmd.js

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -1,43 +1,42 @@
 const {
-    inrl,
-    personalDB,
-    lang
+    plugin,
+    personalDB
 } = require("../lib")
 
-inrl({
+plugin({
     pattern: 'setcmd',
-    desc: lang.MEDIA_CMD.SET_DESC,
+    desc: 'set sticker as command',
     react: "😛",
     type: "action",
     fromMe :true,
     media: "sticker"//you can get this type of active action from 'eval'=>() return lib.commands[0]
 }, async (message, match) => {
-    if (!message.reply_message.msg?.fileSha256) return message.send(lang.MEDIA_CMD.CMD_ERROR)
-    if (!match) return await message.send(lang.MEDIA_CMD.NO_CMD)
+    if (!message.reply_message.msg?.fileSha256) return message.send('_Internal server error!_')
+    if (!match) return await message.send('_which command?!_')
     await personalDB(['sticker_cmd'], {content:{[match]: message.reply_message.msg.fileSha256.join("")}},'add');
-    return await message.reply(lang.BASE.SUCCESS)
+    return await message.reply(`_successfully set command *${match}*_`)
 });
-inrl({
+plugin({
     pattern: 'dltcmd',
-    desc: lang.MEDIA_CMD.DEL_DESC,
+    desc: 'delete a media command',
     react: "💥",
     type: "action",
     fromMe :true
 }, async (message, match) => {
-    if (!match) return await message.send(lang.MEDIA_CMD.NO_CMD)
+    if (!match) return await message.send('_which command?!_')
     await personalDB(['sticker_cmd'], {content:{id: match}},'delete');
-    return await message.reply(lang.BASE.SUCCESS)
+    return await message.reply(`_successfully removed command *${match}*_`)
 });
-inrl({
+plugin({
     pattern: 'getcmd',
-    desc: lang.MEDIA_CMD.GET_DESC,
+    desc: 'get list of sticker command',
     react: "💥",
     type: "action",
     fromMe :true
 }, async (message, match) => {
     const {sticker_cmd} = await personalDB(['sticker_cmd'], {content:{}},'get');
-    if(!Object.keys(sticker_cmd)[0]) return await message.send(lang.MEDIA_CMD.NOT_FOUND);
-    let cmds = lang.MEDIA_CMD.CMD_LIST+'\n\n';
+    if(!Object.keys(sticker_cmd)[0]) return await message.send('_Not Found_');
+    let cmds = '*LIST OF MEDIA COMMAND*' +'\n\n';
     let n = 1;
     for(const cmd in sticker_cmd) {
         cmds += '```'+`${n++}  ${cmd}`+'```'+`\n`
