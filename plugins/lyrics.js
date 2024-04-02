d4d943bf35feb17a3c1002be4a4a2b354f927977
inrl <109638623+INR-L@users.noreply.github.com>
2024-04-02 13:31:04 +0530

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -1,32 +1,32 @@
 //result from musixmatcH 
 const {
-	inrl,
+	plugin,
 	mode,
 	fetchJson,
-	lang,
 	config
 } = require('../lib');
 
 
-inrl({
+plugin({
 	pattern: 'lyrics',
 	fromMe: mode,
-	desc: lang.LYRICS.DESC,
+	desc: 'find lyrics',
 	type: "search"
 }, async (message, match) => {
 	match = match || message.reply_message.text;
-	if (!match) return await message.send(lang.BASE.TEXT);
+	if (!match) return await message.send('_give me a song name!_');
 	const res = await fetchJson(`${config.BASE_URL}api/search/lyrics?text=${match}&apikey=${config.INRL_KEY}`);
-	if (!res.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar inrl_key: your apikey`)
-	if (!res.result) return message.send(lang.BASE.ERROR.format(",try again"));
+	if (!res.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar INRL_KEY: your apikey`)
+	if (!res.result) return message.send('_Internal server Error!_');
 	const {
 		thumb,
 		lyrics,
 		title,
 		artist
-	} = res.result, tbl = "```", tcl = "*_", tdl = "_*";
-	const msg = lang.LYRICS.RESPONCE.format(tcl + artist + tdl, tcl + title + tdl) + `\n\n${tbl}${lyrics}${tbl}`;
-	return await message.client.sendMessage(message.from, {
+	} = res.result;
+	
+	const msg = `*_artist: ${artist}_*\n*_title: ${title}_*\n\n_${lyrics}_`;
+        return await message.client.sendMessage(message.from, {
 		image: {
 			url: thumb
 		},
