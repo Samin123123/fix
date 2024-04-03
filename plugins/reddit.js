3d4bcc2285532c3162f976a1ef6bdbb81a4eb56f
inrl <109638623+INR-L@users.noreply.github.com>
2024-04-03 22:17:11 +0530

    Update reddit.js

--- plugins/reddit.js
+++ plugins/reddit.js
@@ -1,11 +1,11 @@
 const {
-        inrl,
+        plugin,
         mode,
         config,
         getJson
 } = require('../lib');
 
-inrl({
+plugin({
         pattern: 'reddit',
         type: "search",
         fromMe: mode,
@@ -14,7 +14,7 @@ inrl({
                 match = match || message.reply_message.text;
                 if (!match) return await message.send("*please give me an text!*");
                 const data = await getJson(`${config.BASE_URL}api/search/reddit?query=${match}&apikey=${config.INRL_KEY}`);
-                if(!data.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar inrl_key: your apikey`); 
+                if(!data.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar INRL_KEY: your apikey`); 
                 let msg ="*_→RESULT FROM REDDIT←_*\n\n";
                 data.result.map(({title, image, link, nsfw,voteRatio,like,author, dislike,comment,subredditName})=>{
                 msg += `_• title: ${title}_
